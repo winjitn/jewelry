9d208c46b4c7a8590a42085bc24f71bf1ed8a581
win <winjitn@gmail.com>
2020-01-20 12:34:14 +0700

    heroku config

--- config/prod.js
+++ config/prod.js
@@ -1,1 +1,6 @@
-module.exports = {};
+module.exports = {
+  mongoURI2: process.env.mongoURI2,
+  ClientID: process.env.ClientID,
+  ClientSecret: process.env.ClientSecret,
+  RefreshToken: process.env.RefreshToken
+};
